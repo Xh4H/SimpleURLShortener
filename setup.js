452ff03b0e9af1a8ea462d450ecf805b8b00d60c
Xh4H <priciel@protonmail.com>
2019-05-03 12:24:22 +0200

    small woopsie

--- setup.js
+++ setup.js
@@ -1,114 +1,125 @@
-var express = require("express"); 
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-var path = require('path');
-app.use(express.static('public'));
-
+const readlineSync = require('readline-sync');
+const fs = require('fs');
 const mariadb = require('mariadb');
-const dbConfig = JSON.parse(fs.readFileSync('shortener_config.json'));
+const colors = require('colors');
 var crypto = require('crypto');
 
-var pool = mariadb.createPool({
-	host: dbConfig.host,
-	port: dbConfig.port,
-	user: dbConfig.username,
-	password: dbConfig.password,
-	database: dbConfig.database
-});
+// Load DB connection details
+var dbConfig = JSON.parse(fs.readFileSync('shortener_config.json'));
+var hash_length;
 
-const IP = dbConfig.server_ip; // IP where express will run
-const PORT = dbConfig.server_port; // PORT where express will run
+// Functions
+const log = (input) => console.log("[Shortener] " + input);
+const ask = (what, hide) => {
+	return readlineSync.question("[Shortener] " + what, {
+		hideEchoBack: hide != undefined ? hide : false // Hide user input (passwords)
+	})
+};
 
-// ************************************************** //
-// ********************* FUNCTIONS ****************** //
-// ************************************************** //
+function generateStructure(conn) {
+	const DROP_QUERY = "DROP TABLE IF EXISTS \`shortener_urls\`;";
+	const CREATE_QUERY = `CREATE TABLE \`shortener_urls\` (
+			\`id\` int(11) NOT NULL AUTO_INCREMENT,
+			\`hash\` varchar(`+ hash_length + `) NOT NULL,
+			\`url\` varchar(400) NOT NULL,
+			\`ip\` varchar(15) NOT NULL,
+			PRIMARY KEY (\`id\`)
+		);`;
 
-const isValidUrl = (string) => {
-	var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-		'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-		'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-		'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-	return !!pattern.test(string);
+	conn.query(DROP_QUERY);
+	conn.query(CREATE_QUERY)
+		.then(() => {
+			log("Structure created. Exiting...");
+			process.kill(1);
+		})
 }
 
+function modifyDBDetails() {
+	log("DB connection details are wrong. Please fix them.".red);
+	ask("Whenever you have edited 'shortener_config.json' please press ENTER: ");
+	dbConfig = JSON.parse(fs.readFileSync('shortener_config.json'));
+	log("Attempting another connection...".yellow);
 
-// ************************************************** //
-// *************** ROUTE ENDPOINTS ****************** //
-// ************************************************** //
+	openConnection();
+}
 
-pool.getConnection()
-	.then(conn => {
-		function getURL(hash) {
-			return conn.query({
-				rowsAsArray: true,
-				sql: "SELECT url FROM shortener_urls WHERE hash = ?"
-			}, [hash])
-		}
+function openConnection() {
+	log("Needed DB structure will be created.".green);
+	log("Opening a connection to the DB...".green);
 
-		function checkURL(url) {
-			return conn.query({
-				rowsAsArray: true,
-				sql: "SELECT hash FROM shortener_urls WHERE url = ?"
-			}, [url])
-		}
-		app.get('/shorten', function(req, res, next) {
-			let hash = req.query.hash;
-			if (!hash) {
-				res.sendFile(path.join(__dirname + '/shorten.html'));
-			} else {
-				let url;
-				getURL(hash)
-					.then((data) => {
-						if (data[0]) {
-							url = data[0][0];
-						}
-
-						if (url) res.status(301).redirect(url);
-						else res.status(401, "Hash not found");
-					});
-			}
-			
+	createPool()
+		.then((conn) => generateStructure(conn))
+		.catch((e) => {
+			log("An error has occurred.".red)
+			log(e)
+			modifyDBDetails();
 		});
+}
 
-		app.post('/newurl', function(req, res, next) {
-			let url = req.body["url"];
-			let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-			let hash;
-			if (isValidUrl(url)) {
-				checkURL(url)
-					.then((data) => {
-						if (data[0]) {
-							hash = data[0][0];
-						}
-
-						if (hash) res.status(200).send(hash);
-						else {
-							hash = crypto.createHash('sha256').update(url + (+ new Date())).digest("hex").substring(0, 9);
-
-							conn.query({
-								rowsAsArray: true,
-								sql: 'INSERT INTO shortener_urls (hash, url, ip) VALUES (?, ?, ?);'
-							}, [hash, url, ip])
-								.then(() => {
-									res.status(200).send(hash);
-								});
-						}
-					});
-				
-			} else {
-				res.status(200).send("lmao"); // Return lmao for an attempt of sending a "fake" URL
-			}
-		});
-
-		var server = app.listen(PORT, IP, function () {
-			console.log('Server is running..'); 
-		});
-
-		setInterval(function() {
-			conn.query("SELECT 1"); // conn keep-alive
-		}, 60000)
+function createPool() {
+	let pool = mariadb.createPool({
+		host: dbConfig.host,
+		port: dbConfig.port,
+		user: dbConfig.username,
+		password: dbConfig.password,
+		database: dbConfig.database
 	});
+
+	return pool.getConnection();
+}
+
+function askLength() {
+	log("Choose the length of the hash used in shortened URLs.".green);
+	let MAX = 30, MIN = 5, value = 10, key;
+	console.log('\n\n' + (new Array(10)).join(' ') +
+		'[Z] <- -> [X]  WHEN FINISHED PRESS: [SPACE]\n');
+
+	while (true) {
+		console.log('\x1B[1A\x1B[K' +
+			(new Array(13)).join(' ') + "|" +
+			(new Array(value + 1)).join('-') + 'O' +
+			(new Array(MAX - value + 1)).join('-') + '| ' + value);
+
+		key = readlineSync.keyIn('',
+			{hideEchoBack: true, mask: '', limit: 'zx '});
+
+		if (key === 'z') { if (value > MIN) { value--; } }
+		else if (key === 'x') { if (value < MAX) { value++; } }
+		else { break; }
+	}
+
+	return value;
+}
+
+let createStructure;
+let unexpectedInput = false;
+
+while (createStructure !== "" && createStructure !== "y" && createStructure !== "n") {
+	if (unexpectedInput) log("Excuse me, I did not expect that answer...".yellow);
+
+	createStructure = ask("Should I create the database structure? (Y/N) [Y by default]: ").toLowerCase();
+	unexpectedInput = true // Destroyed when the loop is finished
+}
+
+delete unexpectedInput;
+createStructure = !createStructure.includes("n"); // Whether we should create the structure
+
+if (createStructure) {
+	hash_length = parseInt(askLength());
+	let hash = crypto.createHash('sha256').update("someurlgoeshere" + (+ new Date())).digest("hex").substring(0, hash_length);
+	log("Example of a shortened url: https://www.mydomain.com/shorten?hash=" + hash);
+
+	// Update settings
+	dbConfig.length = hash_length;
+	fs.writeFileSync('shortener_config.json', JSON.stringify(dbConfig, null, 4), 'utf8');
+	openConnection();
+}
+
+// Express settings
+dbConfig.server_ip = ask("Type the public IP where the service will run (or leave empty for 'localhost'): ");
+if (dbConfig.server_ip === "") dbConfig.server_ip = "localhost";
+
+let dbConfig.server_port = ask("Type the port where the service will run (or leave empty for '8080'): ");
+if (dbConfig.server_port === "") dbConfig.server_port = 8080;
+
+fs.writeFileSync('shortener_config.json', JSON.stringify(dbConfig, null, 4), 'utf8');
